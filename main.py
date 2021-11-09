d340948285c7e90a4951cf8105599d4adaa1aba9
Chieu Anh Nguyen <59189808+ChieuAnh-Nguyen@users.noreply.github.com>
2021-11-09 15:44:49 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import smtplib
+from config import email_address, email_password
+from email.message import EmailMessage
+
+
+def create_email(Subject, From, To, Content):
+    msg = EmailMessage()
+    msg["Subject"] = Subject
+    msg["From"] = From
+    msg["To"] = To
+    msg.set_content(Content)
+    return msg
+
+
+def send_email(login_email, login_password, msg):
+    """create your login_password at https://myaccount.google.com/apppasswords"""
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smpt:
+        smpt.login(login_email, login_password)
+        smpt.send_message(msg)
+
+
+if __name__ == "__main__":
+    msg = create_email("Test Subject", email_address, email_address, "Test Body")
+    send_email(email_address, email_password, msg)
